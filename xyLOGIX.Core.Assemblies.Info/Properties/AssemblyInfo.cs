9bfc2138158d19d0662e0688f46012a27adebfab
Brian Hart <brian.hart@xylogix.net>
2023-09-06 00:56:02 -0600

    Latest updates at 00:55 hours MDT on 09/06/2023

--- xyLOGIX.Core.Assemblies.Info/Properties/AssemblyInfo.cs
+++ xyLOGIX.Core.Assemblies.Info/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("xyLOGIX Support Library for Assembly Information")]
 [assembly:
     AssemblyCopyright(
-        "Copyright ? 2020-23 by xyLOGIX, LLC.  All rights reserved.")]
+        "Copyright © 2020-23 by xyLOGIX, LLC.  All rights reserved.")]
 [assembly:
     AssemblyTrademark(
         "This software is a component of xyLOGIX Core Assembly Info Library.  xyLOGIX Core Assembly Info Library is a trademark of xyLOGIX, LLC.  All rights reserved, especially those granted by international treaties.")]
