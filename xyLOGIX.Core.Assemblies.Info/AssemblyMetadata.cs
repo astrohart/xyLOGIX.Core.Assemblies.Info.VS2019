0c09a8892477feb1252cc3e1f0386b7506a5b571
Brian Hart <brian.hart@xylogix.net>
2024-10-10 12:45:06 -0600

    Re-encode files with UTF-8 BOM

--- xyLOGIX.Core.Assemblies.Info/AssemblyMetadata.cs
+++ xyLOGIX.Core.Assemblies.Info/AssemblyMetadata.cs
@@ -1,4 +1,4 @@
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using Alphaleonis.Win32.Filesystem;
 using PostSharp.Patterns.Diagnostics;
 using PostSharp.Patterns.Threading;
--- xyLOGIX.Core.Assemblies.Info/AssemblyReferenceRequestedEventHandler.cs
+++ xyLOGIX.Core.Assemblies.Info/AssemblyReferenceRequestedEventHandler.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Reflection;
 
 namespace xyLOGIX.Core.Assemblies.Info
