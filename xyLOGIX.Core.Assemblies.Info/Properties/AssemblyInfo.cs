0fc9277d61c7cb7b3e5b71536121312d771c2ec6
Brian Hart <brian.hart@xylogix.net>
2025-06-22 10:16:15 -0600

    Update AssemblyInfo.cs and project files
    - Update `AssemblyInfo.cs` files to reflect new product names and copyright years.
    - Modify project references in `.csproj` files, updating GUIDs.
    - Update solution file (`.sln`) for project configurations and Visual Studio version.
    - Revise `README.md` to include new method documentation and references.

--- xyLOGIX.Core.Assemblies.Info/Properties/AssemblyInfo.cs
+++ xyLOGIX.Core.Assemblies.Info/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("xyLOGIX Support Library for Assembly Information")]
 [assembly:
     AssemblyCopyright(
-        "Copyright © 2020-25 by xyLOGIX, LLC.  All rights reserved."
+        "Copyright © 2021-25 by xyLOGIX, LLC.  All rights reserved."
     )]
 [assembly:
     AssemblyTrademark(
