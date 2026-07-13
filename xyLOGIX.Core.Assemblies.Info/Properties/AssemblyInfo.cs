e328a40652333b3018ed77763d0768e68c5cfd35
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-07-13 17:19:58 -0600

    Update AssemblyInfo.cs
    - Reflowed assembly metadata comments into wrapped multi-line form.
    - Consolidated the `AssemblyCopyright` attribute onto a single line.
    - Preserved all assembly metadata values, COM visibility, GUID, and version information.

--- xyLOGIX.Core.Assemblies.Info/Properties/AssemblyInfo.cs
+++ xyLOGIX.Core.Assemblies.Info/Properties/AssemblyInfo.cs
@@ -1,7 +1,8 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following set of attributes. Change these attribute values to modify the information associated with an assembly.
+// General Information about an assembly is controlled through the following set of attributes.
+// Change these attribute values to modify the information associated with an assembly.
 [assembly: AssemblyTitle("xyLOGIX.Core.Assemblies.Info")]
 [assembly:
     AssemblyDescription(
@@ -10,21 +11,22 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("xyLOGIX, LLC")]
 [assembly: AssemblyProduct("Assembly Information")]
-[assembly:
-    AssemblyCopyright(
-        "Copyright © 2026 by xyLOGIX, LLC.  All rights reserved."
-    )]
+[assembly: AssemblyCopyright("Copyright © 2026 by xyLOGIX, LLC.  All rights reserved.")]
 [assembly:
     AssemblyTrademark(
         "This software is a component of xyLOGIX Core Assembly Info Library.  xyLOGIX Core Assembly Info Library is a trademark of xyLOGIX, LLC.  All rights reserved, especially those granted by international treaties."
     )]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible to COM components.  If you need to access a type in this assembly from COM, set the ComVisible attribute to true on that type.
+// Setting ComVisible to false makes the types in this assembly not visible to COM components.  If
+// you need to access a type in this assembly from COM, set the ComVisible attribute to true on that
+// type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("dd389a2a-3fc0-4f62-b8d7-4a7baa5b031e")]
 
-// Version information for an assembly consists of the following four values:  Major Version Minor Version Build Number Revision  You can specify all the values or you can default the Build and Revision Numbers by using the '*' as shown below: [assembly: AssemblyVersion("3.0.0.0")]
+// Version information for an assembly consists of the following four values:  Major Version Minor
+// Version Build Number Revision  You can specify all the values or you can default the Build and
+// Revision Numbers by using the '*' as shown below: [assembly: AssemblyVersion("3.0.0.0")]
 [assembly: AssemblyVersion("3.0.0.0")]
