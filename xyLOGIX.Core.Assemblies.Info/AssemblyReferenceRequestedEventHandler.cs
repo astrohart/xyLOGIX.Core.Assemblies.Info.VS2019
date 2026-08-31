c3fe8508fd0bc440712273cf8994ec7c0d871d6f
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-08-31 08:41:47 -0600

    Update AssemblyReferenceRequestedEventHandler.cs
    - Expanded the XML summary for `AssemblyReferenceRequestedEventHandler`.
    - Reformatted the `e` parameter documentation for improved readability.
    - Expanded the return-value documentation describing the selected `Assembly`.

--- xyLOGIX.Core.Assemblies.Info/AssemblyReferenceRequestedEventHandler.cs
+++ xyLOGIX.Core.Assemblies.Info/AssemblyReferenceRequestedEventHandler.cs
@@ -4,8 +4,18 @@ using System.Reflection;
 
 namespace xyLOGIX.Core.Assemblies.Info
 {
-    /// <summary>Delegate that expresses the method signature for a handler of an <c>AssemblyReferenceRequested</c> event.</summary>
-    /// <param name="e">(Required.) An <see cref="T:System.EventArgs" /> that contains the event data.</param>
-    /// <returns>If successful, a reference to an instance of <see cref="T:System.Reflection.Assembly" /> that indicates which .NET assembly is to be used for extracting metadata.</returns>
+    /// <summary>
+    /// Delegate that expresses the method signature for a handler of an
+    /// <c>AssemblyReferenceRequested</c> event.
+    /// </summary>
+    /// <param name="e">
+    /// (Required.) An <see cref="T:System.EventArgs" /> that contains
+    /// the event data.
+    /// </param>
+    /// <returns>
+    /// If successful, a reference to an instance of
+    /// <see cref="T:System.Reflection.Assembly" /> that indicates which .NET assembly
+    /// is to be used for extracting metadata.
+    /// </returns>
     public delegate Assembly AssemblyReferenceRequestedEventHandler([NotLogged] EventArgs e);
 }
