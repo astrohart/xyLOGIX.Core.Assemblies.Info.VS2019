fd2365858f63f8b62491b65632fe1d584d4938d2
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-08-31 08:51:47 -0600

    Update StackFrameExtensions.cs
    - Added a logged static constructor to `StackFrameExtensions`.
    - Expanded the XML documentation for `StackFrameExtensions` and `GetDeclaringAssembly`.
    - Applied `Log(AttributeExclude = true)` to `GetDeclaringAssembly`.
    - Applied `NotLogged` to the `GetDeclaringAssembly` return value and `frame` parameter.

--- xyLOGIX.Core.Assemblies.Info/StackFrameExtensions.cs
+++ xyLOGIX.Core.Assemblies.Info/StackFrameExtensions.cs
@@ -6,14 +6,48 @@ using xyLOGIX.Core.Debug;
 
 namespace xyLOGIX.Core.Assemblies.Info
 {
-    /// <summary>Exposes extension methods for the <see cref="T:System.Diagnostics.StackFrame" /> class.</summary>
+    /// <summary>
+    /// Exposes extension methods for the
+    /// <see cref="T:System.Diagnostics.StackFrame" /> class.
+    /// </summary>
     [Log(AttributeExclude = true)]
     public static class StackFrameExtensions
     {
-        /// <summary>Attempts to obtain a reference to the <see cref="T:System.Reflection.Assembly" /> instance that represents the assembly that contains the method that is at the current stack <paramref name="frame" />.</summary>
-        /// <param name="frame">(Required.) Reference to an instance of the <see cref="T:System.Diagnostics.StackFrame" /> that represents the top of the call stack.</param>
-        /// <returns>If successful, a reference to the <see cref="T:System.Reflection.Assembly" /> instance that represents the assembly that contains the method that is at the current stack <paramref name="frame" />; <see langword="null" /> otherwise.</returns>
-        public static Assembly GetDeclaringAssembly(this StackFrame frame)
+        /// <summary>
+        /// Initializes <see langword="static" /> data or performs actions that
+        /// need to be performed once only for the
+        /// <see cref="T:xyLOGIX.Core.Assemblies.Info.StackFrameExtensions" /> class.
+        /// </summary>
+        /// <remarks>
+        /// This constructor is called automatically prior to the first instance
+        /// being created or before any <see langword="static" /> members are referenced.
+        /// <para />
+        /// We've decorated this constructor with the <c>[Log(AttributeExclude = true)]</c>
+        /// attribute in order to simplify the logging output.
+        /// </remarks>
+        [Log(AttributeExclude = true)]
+        static StackFrameExtensions() { }
+
+        /// <summary>
+        /// Attempts to obtain a reference to the
+        /// <see cref="T:System.Reflection.Assembly" /> instance that represents the
+        /// assembly that contains the method that is at the current stack
+        /// <paramref name="frame" />.
+        /// </summary>
+        /// <param name="frame">
+        /// (Required.) Reference to an instance of the
+        /// <see cref="T:System.Diagnostics.StackFrame" /> that represents the top of the
+        /// call stack.
+        /// </param>
+        /// <returns>
+        /// If successful, a reference to the
+        /// <see cref="T:System.Reflection.Assembly" /> instance that represents the
+        /// assembly that contains the method that is at the current stack
+        /// <paramref name="frame" />; <see langword="null" /> otherwise.
+        /// </returns>
+        [Log(AttributeExclude = true)]
+        [return: NotLogged]
+        public static Assembly GetDeclaringAssembly([NotLogged] this StackFrame frame)
         {
             Assembly result = default;
 
