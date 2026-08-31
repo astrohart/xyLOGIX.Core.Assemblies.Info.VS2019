8640d19db2c9d556229579be5a7c235cb86a0bdf
Brian C. Hart, Ph.D. <brian.hart@xylogix.net>
2026-08-31 08:42:03 -0600

    Update Find.cs
    - Expanded the XML documentation for the `Find` class.
    - Reformatted documentation for `AllAssembliesThatDependOn` and its `executingAssembly` parameter.
    - Clarified the parameter, return-value, and failure-condition documentation for `DependsOn`.

--- xyLOGIX.Core.Assemblies.Info/Find.cs
+++ xyLOGIX.Core.Assemblies.Info/Find.cs
@@ -9,13 +9,27 @@ using xyLOGIX.Core.Debug;
 
 namespace xyLOGIX.Core.Assemblies.Info
 {
-    /// <summary>Exposes <see langword="static" /> methods to find information on assemblies through Reflection.</summary>
+    /// <summary>
+    /// Exposes <see langword="static" /> method(s) to find information on
+    /// assemblies through Reflection.
+    /// </summary>
     [Log(AttributeExclude = true)]
     public static class Find
     {
-        /// <summary>Attempts to obtain a collection of references to instances of <paramref name="executingAssembly" /> in the call stack that refer to the specified <paramref name="executingAssembly" />.</summary>
-        /// <param name="executingAssembly">(Required.) Reference to an instance of the currently-executing <see cref="T:System.Reflection.Assembly" />.</param>
-        /// <returns>If successful, a read-only collection of references to instances of <paramref name="executingAssembly" /> in the call stack that refer to the specified <paramref name="executingAssembly" /> is returned.</returns>
+        /// <summary>
+        /// Attempts to obtain a collection of references to instances of
+        /// <paramref name="executingAssembly" /> in the call stack that refer to the
+        /// specified <paramref name="executingAssembly" />.
+        /// </summary>
+        /// <param name="executingAssembly">
+        /// (Required.) Reference to an instance of the
+        /// currently-executing <see cref="T:System.Reflection.Assembly" />.
+        /// </param>
+        /// <returns>
+        /// If successful, a read-only collection of references to instances of
+        /// <paramref name="executingAssembly" /> in the call stack that refer to the
+        /// specified <paramref name="executingAssembly" /> is returned.
+        /// </returns>
         /// <remarks>If there is an issue that is experienced</remarks>
         public static IReadOnlyList<Assembly> AllAssembliesThatDependOn(Assembly executingAssembly)
         {
@@ -46,11 +60,28 @@ namespace xyLOGIX.Core.Assemblies.Info
             return result;
         }
 
-        /// <summary>Determines if the specified <paramref name="executingAssembly" /> is referred to by other assemblies in the current stack trace frame.</summary>
-        /// <param name="currentAssembly">(Required.) A <see cref="T:System.Reflection.Assembly" /> that is to be checked.</param>
-        /// <param name="executingAssembly">(Required.) Reference to the currently-executing <see cref="T:System.Reflection.Assembly" />.</param>
-        /// <returns><see langword="true" /> if the specified <paramref name="currentAssembly" /> depends upon the <paramref name="executingAssembly" />; <see langword="false" /> otherwise, or if the relationship between the specified assemblies could not be determined.</returns>
-        /// <remarks>This method also returns <see langword="false" /> if information is missing or a system error occurs during the operation.</remarks>
+        /// <summary>
+        /// Determines if the specified <paramref name="executingAssembly" /> is
+        /// referred to by other assemblies in the current stack trace frame.
+        /// </summary>
+        /// <param name="currentAssembly">
+        /// (Required.) A
+        /// <see cref="T:System.Reflection.Assembly" /> that is to be checked.
+        /// </param>
+        /// <param name="executingAssembly">
+        /// (Required.) Reference to the
+        /// currently-executing <see cref="T:System.Reflection.Assembly" />.
+        /// </param>
+        /// <returns>
+        /// <see langword="true" /> if the specified
+        /// <paramref name="currentAssembly" /> depends upon the
+        /// <paramref name="executingAssembly" />; <see langword="false" /> otherwise, or
+        /// if the relationship between the specified assemblies could not be determined.
+        /// </returns>
+        /// <remarks>
+        /// This method also returns <see langword="false" /> if information is
+        /// missing or a system error occurs during the operation.
+        /// </remarks>
         private static bool DependsOn(this Assembly currentAssembly, Assembly executingAssembly)
         {
             var result = false;
